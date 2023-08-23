06dd448b65ffb2ef849062d901b273892ebf1d23
yangxy <yangxianyin@mail.taiji.com.cn>
2023-08-23 19:54:42 +0800

    修改项目名称及vite.config.ts中报错的内容

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
     eslintPlugin(),
-    viteMockServe({ mockPath: 'mock', enable: true })
+    viteMockServe({ mockPath: 'mock', localEnabled: true })
   ],
   base: './',
   resolve: {
